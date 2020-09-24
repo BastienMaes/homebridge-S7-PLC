3dfdbc7ca9e469fddd8744b05a601556eb85b025
Feilner <github@oliver.4of.de>
2020-09-24 23:45:58 +0200

    add blind

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ var S7accessory = {
     "S7_Sensor" : Sensor,
     "S7_Humidity" : Sensor,
     "S7_Temperature" : Sensor,
-    "S7_Thermostat" : Thermostat
+    "S7_Thermostat" : Thermostat,
+    "S7_WindowCovering": WindowCovering
 }//this var is used to have a unique constructor for accessories instanciation in platform
 
 //Exports
@@ -33,6 +34,7 @@ module.exports = function(homebridge) {
   homebridge.registerAccessory(platformName, 'S7_Humidity', Sensor, true);
   homebridge.registerAccessory(platformName, 'S7_Temperature', Sensor, true);  
   homebridge.registerAccessory(platformName, 'S7_Thermostat', Thermostat, true);  
+  homebridge.registerAccessory(platformName, 'S7_WindowCovering', WindowCovering, true);    
 }
 
 //Platform definitions
@@ -648,11 +650,7 @@ function Thermostat(platform, config) {
 Thermostat.prototype = {
 
   getServices: function() {
-    var informationService = new Service.AccessoryInformation();
-    informationService
-    .setCharacteristic(Characteristic.Manufacturer, "EQ-3")
-    .setCharacteristic(Characteristic.Model, "Max! Cube")
-    return [informationService, this.service];
+    return [this.accessory.getService(Service.AccessoryInformation), this.service];
   },
 
 
@@ -687,7 +685,33 @@ Thermostat.prototype = {
     }
   },
   
+  setValue: function(callback, offset, value) {
+    var platform = this.platform;    
+    var S7Client = this.platform.S7Client;
 
+    //check PLC connection
+    platform.S7ClientReconnect();
+    
+    if (S7Client.Connected()) {
+        this.buf.writeFloatBE(value, 0);
+        // Read one real from DB asynchonousely...
+        S7Client.WriteArea(S7Client.S7AreaDB, this.db, offset, 1, S7Client.S7WLReal, this.buf, function(err) {
+          if(err) {
+            this.log(' >> DBWritefailed (DB' + this.db + '.DBD' + dbbyte + '):. Code #' + err + ' - ' + S7Client.ErrorText(err));
+            S7Client.Disconnect();
+            value = 0.0;
+            callback(err);
+          }
+          else {
+            
+            callback(null);
+          }
+        });
+    }
+    else {
+        callback(new Error('PLC not connected'), this.minValue);
+    }
+  },
 
   /**
    * Handle requests to get the current value of the "Current Heating Cooling State" characteristic
@@ -696,7 +720,7 @@ Thermostat.prototype = {
     this.log('Triggered GET CurrentHeatingCoolingState');
 
     // set this to a valid value for CurrentHeatingCoolingState
-    const currentValue = 1;
+    const currentValue = 3;
 
     callback(null, currentValue);
   },
@@ -709,7 +733,7 @@ Thermostat.prototype = {
     this.log('Triggered GET TargetHeatingCoolingState');
 
     // set this to a valid value for TargetHeatingCoolingState
-    const currentValue = 1;
+    const currentValue = 3;
 
     callback(null, currentValue);
   },
@@ -747,6 +771,114 @@ Thermostat.prototype = {
    */
   handleTargetTemperatureSet: function(value, callback) {
     this.log('Triggered SET TargetTemperature %s:', value);
+    this.setValue(callback, this.setTargetTempOffset, value);
+  },
+
+  /**
+   * Handle requests to set the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsSet: function(value, callback) {
+    this.log('Triggered SET TemperatureDisplayUnits %s:', value);
+
+    callback(null);
+  },
+  
+  
+    /**
+   * Handle requests to get the current value of the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsGet(callback) {
+    this.log.debug('Triggered GET TemperatureDisplayUnits');
+
+    // set this to a valid value for TemperatureDisplayUnits
+    const currentValue = 0;
+
+    callback(null, currentValue);
+  }
+}
+
+
+
+
+
+
+function WindowCovering(platform, config) {
+    this.FirmwareRevision = '0.0.1';
+    this.platform = platform;
+    this.log = platform.log;
+    this.name = config.name;
+    this.manufacturer = ('manufacturer' in config) ? config.manufacturer : 'S7-PLC';
+    this.db = config.DB;
+    this.getCurrentPositionOffset = config.getCurrentPositionOffset
+    this.getTargetPositionOffset = config.getTargetPositionOffset;
+    this.setTargetPositionOffset  = config.setTargetPositionOffset;
+    this.buf = Buffer.alloc(4);
+    var uuid = UUIDGen.generate("this.name + 'WindowCovering'");
+    this.accessory = new Accessory(this.name, uuid);    
+    this.service = new Service.WindowCovering(this.name);
+    this.accessory.addService(this.service);
+
+    this.accessory.getService(Service.AccessoryInformation)
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, 'S7-WindowCovering')
+      .setCharacteristic(Characteristic.SerialNumber, uuid)
+      .setCharacteristic(Characteristic.FirmwareRevision, this.FirmwareRevision); 
+      
+      // create handlers for required characteristics
+      this.service.getCharacteristic(Characteristic.CurrentPosition)
+        .on('get', this.handleCurrentPositionGet.bind(this));
+
+      this.service.getCharacteristic(Characteristic.TargetPosition)
+        .on('get', this.handleTargetPositionGet.bind(this))
+        .on('set', this.handleTargetPositionSet.bind(this));
+
+      this.service.getCharacteristic(Characteristic.PositionState)
+        .on('get', this.handlePositionStateGet.bind(this));
+        
+      this.service.getCharacteristic(Characteristic.HoldPosition)
+        .on('set', this.handleHoldPosition.bind(this));
+        
+    this.log("Starting a S7_Thermostat Service '" + this.name + "' on DB%d.DBD%d", this.db, this.dbbyte);
+}
+
+WindowCovering.prototype = {
+
+  getServices: function() {
+    return [this.accessory.getService(Service.AccessoryInformation), this.service];
+  },
+
+  getValue: function(callback, offset) {
+    var log = this.log;
+    var platform = this.platform;    
+    var S7Client = this.platform.S7Client;
+    debugSensor("getCurrentValue: START");
+
+    //check PLC connection
+    this.platform.S7ClientReconnect();
+    
+    if (S7Client.Connected()) {
+        // Read one real from DB asynchonousely...
+        S7Client.ReadArea(S7Client.S7AreaDB, this.db, offset, 1, S7Client.S7WLReal, function(err, res) {
+          if(err) {
+            log('WindowCovering: ERROR >> DBRead failed (DB' + this.db + '.DBD' + offset + '):. Code #' + err + ' - ' + S7Client.ErrorText(err));
+            S7Client.Disconnect();
+            callback(err, null);
+          }
+          else {
+            log("WindowCovering: getValue: DB%d.DBD%d: %f", this.db, offset, res.readFloatBE(0));
+            var value = res.readFloatBE(0);
+            callback(null, value);
+          }
+        });
+        
+      debugSensor("getCurrentValue: END");
+    }
+    else {
+        callback(new Error('PLC not connected'), null);
+    }
+  },
+
+  setValue: function(callback, offset, value) {
     var platform = this.platform;    
     var S7Client = this.platform.S7Client;
 
@@ -756,9 +888,9 @@ Thermostat.prototype = {
     if (S7Client.Connected()) {
         this.buf.writeFloatBE(value, 0);
         // Read one real from DB asynchonousely...
-        S7Client.WriteArea(S7Client.S7AreaDB, this.db, this.setTargetTempOffset, 1, S7Client.S7WLReal, this.buf, function(err) {
+        S7Client.WriteArea(S7Client.S7AreaDB, this.db, offset, 1, S7Client.S7WLReal, this.buf, function(err) {
           if(err) {
-            this.log(' >> DBWritefailed (DB' + this.db + '.DBD' + dbbyte + '):. Code #' + err + ' - ' + S7Client.ErrorText(err));
+            this.log('WindowCovering: ERROR >> DBWrite failed (DB' + this.db + '.DBD' + dbbyte + '):. Code #' + err + ' - ' + S7Client.ErrorText(err));
             S7Client.Disconnect();
             value = 0.0;
             callback(err);
@@ -768,32 +900,52 @@ Thermostat.prototype = {
             callback(null);
           }
         });
-        
-
-
     }
     else {
         callback(new Error('PLC not connected'), this.minValue);
     }
   },
 
-  /**
-   * Handle requests to get the current value of the "Temperature Display Units" characteristic
-   */
-  handleTemperatureDisplayUnitsGet: function(callback) {
-    this.log('Triggered GET TemperatureDisplayUnits');
 
-    // set this to a valid value for TemperatureDisplayUnits
-    const currentValue = 1;
+  handleCurrentPositionGet(callback) {
+    this.log('WindowCovering: Triggered GET CurrentPosition');
+    this.getValue(callback, this.getCurrentPositionOffset);    
+  },
+
+  /**
+   * Handle requests to get the current value of the "Target Position" characteristic
+   */
+  handleTargetPositionGet(callback) {
+    this.log('WindowCovering: Triggered GET TargetPosition');
+    this.getValue(callback, this.getTargetPositionOffset);
+  },
+
+  /**
+   * Handle requests to set the "Target Position" characteristic
+   */
+  handleTargetPositionSet(value, callback) {
+    this.log('WindowCovering: Triggered SET TargetPosition: %s', value);
+    this.setValue(callback, this.setTargetPositionOffset, value);
+  },
+
+  /**
+   * Handle requests to get the current value of the "Position State" characteristic
+   */
+  handlePositionStateGet(callback) {
+    this.log('WindowCovering: Triggered GET PositionState');
+
+    // set this to a valid value for PositionState
+    const currentValue = 2;
 
     callback(null, currentValue);
   },
 
+
   /**
-   * Handle requests to set the "Temperature Display Units" characteristic
+   * Handle requests to set the "Hold Position" characteristic
    */
-  handleTemperatureDisplayUnitsSet: function(value, callback) {
-    this.log('Triggered SET TemperatureDisplayUnits %s:', value);
+  handleHoldPosition: function(callback) {
+    this.log('WindowCovering: Triggered SET HoldPositionState');
 
     callback(null);
   }
